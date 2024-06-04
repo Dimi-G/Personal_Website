13cde749dff4f196b05a8b9f596bc17737bb6de0
Dimi-G <dim.gkogkou@gmail.com>
2024-06-04 13:01:42 +0200

    sidebar

--- sidebar.py
+++ sidebar.py
@@ -4,5 +4,5 @@ def show_sidebar():
     with st.container():
         st.sidebar.image("static/DG.jpg")
         st.sidebar.header("Dr. Dimitra Gkogkou")
-        st.sidebar.write("Aspiring Data Scientist  \n Physicist  \n Scientific Editor  \n")
+        st.sidebar.write("Data Scientist  \n Physicist  \n Scientific Editor  \n")
 
